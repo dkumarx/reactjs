85b5f469ccd0f8cb06a923d0794dd093b4e4eb1d
DhananjayKumar <dkumar4it@gmail.com>
2019-07-21 00:15:56 +1000

    Added REST API data calling and update DOM accordingly

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ let personalData = {
   firstName: "Dhananjay",
   lastName: "Kumar",
   Address: "South Melbourne, VIC",
-  age: 40
+  age: 41
 }
 
 //Library 
@@ -15,42 +15,78 @@ let BookList = [
    {"title": "The Myth", "author": "Charls", "pages": 200}
 ]
 
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
    return (
       <section>
          <h2>{title}</h2>
          <p>by: {author}</p>
          <p>Pages: {pages} pages</p>
+         <p>Free Bookmark today: {freeBookmark ? 'Yes' : 'No'}</p>
       </section>
    )
 }
 
+// Create functional component W/O retun 
+const Available = () => <div>Self checkout is Available today to borrow!!</div>
+const NotAvailable = () => <div>Self checkout is NOT available today!!</div>
+
 class Library extends Component {
-   constructor (props) {
-      super(props); // create new instance of class
-      this.state = {
-         open: true
-      }
+   // Initiallizing state as ES6 pattern
+   state = { 
+      open: true,
+      freeBookmark: false, 
+      available: true,
+      data: [],
+      loading: false
+   }
+   // Change state based on prevState so that it will maintain state 
+   toggleOpenClosed = () => {
+      this.setState(prevState => ({
+         open: !prevState.open
+      }))
    }
 
-   toggleOpenClosed() {
-      this.setState({
-         open: !this.state.open
-      })
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+      this.setState({loading: true})
+      fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+          .then(data => data.json())
+          .then(data => this.setState({data, loading:false}))
    }
 
    render () {
       const {books} = this.props;
       return (
             <div>
+               {/* Display products from REST API */}
+                  {
+                     this.state.loading
+                     ? "loading..."
+                     : <article>
+                        {
+                           this.state.data.map(product => {
+                              return (
+                                 <div key={product.id}>
+                                    <h3>Library Product of the week!</h3>
+                                    <h4>{product.name}</h4>
+                                    <img src={product.image} height={100} alt="" />
+                                 </div>
+                              )
+                           })
+                        }<hr />
+                     </article>
+                     
+                  }
                <h1>The librry is {this.state.open ? 'open' : 'closed'}</h1>
+               <h2>{this.state.available ? <Available /> : <NotAvailable />}</h2>
                <button onClick={this.toggleOpenClosed.bind(this)}>Change</button>
                {books.map(
                   (book, i) => <Book 
                      key={i}
                      title={book.title}
                      author={book.author}
-                     pages={book.pages} />
+                     pages={book.pages}
+                     freeBookmark={this.state.freeBookmark} />
                )}
             </div>
          ) 
@@ -83,7 +119,7 @@ class App extends Component {
   }
   render() {
      return (
-       <section>
+       <section  style={{padding: "20px"}}>
            <Content 
               myNumber = {this.state.data}
               firstName={personalData.firstName}
@@ -105,6 +141,7 @@ class Content extends Component {
   */
 
   // Order #1:: Execute On load
+  // @depecreated : componentWillMount is deprecated in react 16.3 and stop working in 17.0
   componentWillMount() {
      console.log('Component WILL MOUNT!')
   }
@@ -113,6 +150,7 @@ class Content extends Component {
      console.log('Component DID MOUNT!')
   }
   // Order #3
+  // @depecreated : componentWillReceiveProps is deprecated in react 16.3 and stop working in 17.0
   componentWillReceiveProps(newProps) {    
      console.log('Component WILL RECIEVE PROPS!')
   }
@@ -122,6 +160,7 @@ class Content extends Component {
      return true;
   }
   // Order #5
+  // @depecreated : componentWillUpdate is deprecated in react 16.3 and stop working in 17.0
   componentWillUpdate(nextProps, nextState) {
      console.log('Component WILL UPDATE!');
   }
